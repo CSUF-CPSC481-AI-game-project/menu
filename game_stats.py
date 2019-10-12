886a6b828fd8cdc553b28346ad77abf6cd54ccd5
OsirisLambert <35780056+OsirisLambert@users.noreply.github.com>
2019-10-12 11:35:27 -0700

    Delete game_stats.py

--- game_stats.py
+++
@@ -1,7 +0,0 @@
-class GameStats():
-    def __init__(self, main_settings):
-        self.main_settings = main_settings
-        self.reset_stats()
-        self.game_active = False
-        self.play_again = True
-        self.game_over = False
